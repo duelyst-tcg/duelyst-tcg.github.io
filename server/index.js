b9bbbad1836ade698e59227e5d3fe62a3cbc9c3c
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-06-09 16:39:14 +0200

    Update dev server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
-const path = require('path')
-const express = require('express')
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
 
-const app = express()
-const port = 3000
-const dir = path.join(__dirname, '../cards/')
+var baseDirectory = path.join(__dirname, '../cards/')
+var port = 3000
 
-app.use('/', express.static(dir))
+var server = http.createServer(function (request, response) {
+  try {
+    var requestUrl = url.parse(request.url)
+    var fsPath = baseDirectory + path.normalize(requestUrl.pathname)
+    var fileStream = fs.createReadStream(fsPath)
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+    fileStream.pipe(response)
+
+    fileStream.on('open', function () {
+      response.writeHead(200)
+    })
+    
+    fileStream.on('error', function (e) {
+      response.writeHead(404)
+      response.end()
+    })
+  } catch (e) {
+    response.writeHead(500)
+    response.end()
+    console.log(e.stack)
+  }
 })
+
+server.listen(port)
+console.log("Server listening on port " + port)
